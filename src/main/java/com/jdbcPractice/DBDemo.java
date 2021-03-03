03d0d9f76bec6b9b9d9764ab9aa7ac7bf1214325
Admin <patilsharad2000@gmail.co>
2021-03-03 12:29:16 +0530

    Ability to create Payroll service database and have java program to connect database

---
+++ src/main/java/com/jdbcPractice/DBDemo.java
@@ -0,0 +1,37 @@
+package com.jdbcPractice;
+
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.util.Enumeration;
+
+public class DBDemo {
+    public static void main(String[] args) {
+        String jdbcURL = "jdbc:mysql://localhost:3306/payroll_service?useSSL=false";
+        String userName = "root";
+        String password = "Sharad@1998";
+        Connection connection;
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            System.out.println("Driver Loaded!");
+        } catch (ClassNotFoundException e) {
+            throw new IllegalStateException("Cannot find thr driver in the class path!!", e);
+        }
+        listDrivers();
+        try {
+            System.out.println("Connecting tO database:"+jdbcURL);
+            connection =  DriverManager.getConnection(jdbcURL, userName, password);
+            System.out.println("Connection is Successful!!!!!"+connection);
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+    private static void listDrivers() {
+
+        Enumeration<Driver> driverList = DriverManager.getDrivers();
+        while(driverList.hasMoreElements()) {
+            Driver driverClass = (Driver) driverList.nextElement();
+            System.out.println("   "+driverClass.getClass().getName());
+        }
+    }
+}
